cda8b4792db2e6cdd43abdc742018b33578fcecc
busybrain3053 <116880512+busybrain3053@users.noreply.github.com>
2022-11-05 18:09:30 +0000

    Add files via upload

---
+++ debugger.py
@@ -0,0 +1,4 @@
+name = "john"
+print("Hello " + name +",")
+age = 40
+print(name + "is" + str(age) + "years old.")
---
+++ sys-admin.py
@@ -0,0 +1,15 @@
+import os
+os.system("ls")
+
+import subprocess
+subprocess.run(["ls"])
+
+subprocess.run(["ls","-1","README.md"])
+command= "uname"
+commandArgument= "-a"
+print(f'Gathering system information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
+command= "ps"
+commandArgument= "x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
